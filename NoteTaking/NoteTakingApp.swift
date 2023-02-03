b94381f6bdff83805b6055e25720ea6ecd0b95a4
Tedtya Rady <tedtyarady@gmail.com>
2023-02-03 18:42:10 +1100

    Initial Commit

---
+++ NoteTaking/NoteTakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteTakingApp.swift
+//  NoteTaking
+//
+//  Created by Tedtya rady on 3/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoteTakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
